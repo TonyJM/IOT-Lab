3791d7be323f8385a00c0c02afb940b3717b5130
TonyJM <53893112+TonyJM@users.noreply.github.com>
2019-10-01 15:17:33 +0530

    Add files via upload

---
+++ rfid.ino
@@ -0,0 +1,26 @@
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(9,10);
+
+int count=0;
+char input[12];
+boolean flag=0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(mySerial.available()){
+    count=0;
+    while(mySerial.available() && count<12){
+      input[count]=mySerial.read();
+      count++;
+      delay(5);
+    }
+    Serial.println(input);
+  }
+}
